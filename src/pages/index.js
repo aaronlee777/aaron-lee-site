b4ec2bd6189ab374c50fb8fb9fad71ccc17b817e
Aaron Lee <alee@Aaron-Lees-MacBook-Pro.local>
2021-03-15 15:28:06 -0400

    initial setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,18 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
+  <div>
+    <SEO title="DUTY" />
+    <div className="py-6 px-6">
+      <h3 className="text-gray-800">Hello, I'm Aaron.</h3>
+      <p className='text-gray-700'>
+        I'm currently a Senior Product Designer at at{" "}
+        <a className="" href="www.pagerduty.com">PagerDuty</a>
+      </p>
+    </div>
+  </div>
 )
 
 export default IndexPage
